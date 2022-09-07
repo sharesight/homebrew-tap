effda132f1a8e02e5fd0a9034ea49cd7a2bc8eac
Thorsten Boettger <boettger@mt7.de>
2022-09-07 17:34:25 +1200

    brew extract --version 1.3.2 opensearch sharesight/tap

--- Formula/opensearch@1.3.2.rb
+++ Formula/opensearch@1.3.2.rb
@@ -1,18 +1,9 @@
-class OpensearchAt132 < Formula
+class OpensearchAT132 < Formula
   desc "Open source distributed and RESTful search engine"
   homepage "https://github.com/opensearch-project/OpenSearch"
   url "https://github.com/opensearch-project/OpenSearch/archive/1.3.2.tar.gz"
   sha256 "3927332336e10e7a3c8fb2c8976200d313dcace6358d6a90f5221d6276f17df7"
   license "Apache-2.0"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "82c9268efa73564391f6b99ea0c37d7d48c3a61f3652c275f499c31c57a11e2c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "82c9268efa73564391f6b99ea0c37d7d48c3a61f3652c275f499c31c57a11e2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, catalina:       "cf84778e6ec1b476640697735bcad761f0594b6c3b4a931c2841440b19aa31d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac5d9a44a6f2577800a443da819f9de9d5c0e9d71c45c149102cc61876e2e82e"
-  end
 
   depends_on "gradle@6" => :build
   depends_on "openjdk"
